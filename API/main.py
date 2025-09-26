233e338bd92292389bf855926c5baf3f2ed628b8
leelamadhav07 <srinivasraosunkara846@gmail.com>
2025-09-26 21:17:38 +0530

    project done

--- API/main.py
+++ API/main.py
@@ -39,4 +39,4 @@ def get_clip(clip_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("API.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("API.main:app", host="0.0.0.0", port=8000)
